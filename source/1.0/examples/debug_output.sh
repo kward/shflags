120b504737b67da62a14f31e836261c8fa6eb654
kate.ward <kate.ward@forestent.com>
2008-06-27 08:58:03 +0000

    added copyright, license, and a description

--- source/1.0/examples/debug_output.sh
+++ source/1.0/examples/debug_output.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+#
+# Copyright 2008 Kate Ward. All Rights Reserved.
+# Released under the LGPL (GNU Lesser General Public License)
+#
+# Author: kate.ward@forestent.com (Kate Ward)
+#
+# This script does the very simple job of echoing some text. If a '-d' (or
+# '--debug') flag is given, additinal "debug" output is enabled.
+#
+# This script demonstrates the use of a boolean flag to enable custom
+# functionality in a script.
 
 # source shflags
 . ../src/shflags
