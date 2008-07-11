1d0ecc466906f1db8c1f2fb5fff9b13f9b2f8ce2
kate.ward <kate.ward@forestent.com>
2008-07-11 15:33:23 +0000

    fixed problems with help; changed zsh checks

--- source/1.0/src/shflags_test_parsing.sh
+++ source/1.0/src/shflags_test_parsing.sh
@@ -13,6 +13,9 @@
 # load test helpers
 . ./shflags_test_helpers
 
+# set shwordsplit for zsh
+[ -n "${ZSH_VERSION:-}" ] && setopt shwordsplit
+
 #------------------------------------------------------------------------------
 # suite tests
 #
