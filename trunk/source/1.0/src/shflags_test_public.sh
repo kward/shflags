24c1e3ab04b9b948af720fc036437a3735f1302e
kate.ward@forestent.com <kate.ward@forestent.com@9d7cb843-df4f-0410-8a79-4785ae5a3405>
2008-10-21 19:19:10 +0000

    line wrapping and extra code cleanup

--- trunk/source/1.0/src/shflags_test_public.sh
+++ trunk/source/1.0/src/shflags_test_public.sh
@@ -40,9 +40,7 @@ testHelp()
   rslt=`FLAGS_HELP='this is a test'; FLAGS -h 2>&1`
   echo "${rslt}" |grep -- 'this is a test' >/dev/null
   rtrn=$?
-  assertTrue \
-      'setting FLAGS_HELP did not produce expected result' \
-      ${rtrn}
+  assertTrue 'setting FLAGS_HELP did not produce expected result' ${rtrn}
   [ ${rtrn} -ne ${FLAGS_TRUE} ] && echo "${rslt}" >&2
 
   # test that "'" chars work in help string
@@ -50,9 +48,7 @@ testHelp()
   rslt=`FLAGS -h 2>&1`
   echo "${rslt}" |grep -- "help string containing a ' char" >/dev/null
   rtrn=$?
-  assertTrue \
-      "help strings containing apostrophes don't work" \
-      ${rtrn}
+  assertTrue "help strings containing apostrophes don't work" ${rtrn}
   [ ${rtrn} -ne ${FLAGS_TRUE} ] && echo "${rslt}" >&2
 
   #
@@ -62,14 +58,9 @@ testHelp()
   flags_getoptIsStd && startSkipping
 
   rslt=`FLAGS --help 2>&1`
-  rtrn=$?
-  assertFalse \
-      'long request for help should have returned non-zero exit code.' \
-      ${rtrn}
+  assertFalse 'long help request should have returned non-zero exit code' $?
   echo "${rslt}" |grep -- 'show this help' >/dev/null
-  assertTrue \
-      'long request for help should have produced some help output.' \
-      $?
+  assertTrue 'long help request should have produced some help output.' $?
 }
 
 testStandardHelpOutput()
@@ -95,10 +86,8 @@ flags:
 EOF
   ( FLAGS_HELP=${help}; FLAGS -h >"${stdoutF}" 2>"${stderrF}" )
   diff "${expectedF}" "${stderrF}" >/dev/null
-  rtrn=$?
-  assertTrue 'unexpected help output' ${rtrn}
-  [ ${__shunit_skip} -eq ${SHUNIT_FALSE} \
-    -a ${rtrn} -ne ${FLAGS_TRUE} ] \
+  assertTrue 'unexpected help output' $?
+  [ ${__shunit_skip} -eq ${SHUNIT_FALSE} -a ${rtrn} -ne ${FLAGS_TRUE} ] \
       && cat "${stderrF}"
 }
 
@@ -125,8 +114,7 @@ flags:
 EOF
   ( FLAGS_HELP=${help}; FLAGS -h >"${stdoutF}" 2>"${stderrF}" )
   diff "${expectedF}" "${stderrF}" >/dev/null
-  rtrn=$?
-  assertTrue 'unexpected help output' ${rtrn}
+  assertTrue 'unexpected help output' $?
   [ ${__shunit_skip} -eq ${SHUNIT_FALSE} -a ${rtrn} -ne ${FLAGS_TRUE} ] \
       && cat "${stderrF}"
 }
