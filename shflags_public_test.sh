ef7daa5941a24b6690245282bc050377883461f1
Kate Ward <kate.ward@forestent.com>
2017-10-18 00:16:36 +0200

    Ran shflags_public_test.sh through ShellCheck.

--- shflags_public_test.sh
+++ shflags_public_test.sh
@@ -2,29 +2,39 @@
 # vim:et:ft=sh:sts=2:sw=2
 #
 # shFlags unit test for the public functions.
+#
+# Copyright 2008-2017 Kate Ward. All Rights Reserved.
+# Released under the Apache 2.0 license.
+#
+# Author: kate.ward@forestent.com (Kate Ward)
+# https://github.com/kward/shflags
+#
+### ShellCheck (http://www.shellcheck.net/)
+# Disable source following.
+#   shellcheck disable=SC1090,SC1091
 
-# load test helpers
+# These variables will be overridden by the test helpers.
+expectedF="${TMPDIR:-/tmp}/expected"
+returnF="${TMPDIR:-/tmp}/return"
+stdoutF="${TMPDIR:-/tmp}/STDOUT"
+stderrF="${TMPDIR:-/tmp}/STDERR"
+
+# Load test helpers.
 . ./shflags_test_helpers
 
-#------------------------------------------------------------------------------
-# Suite tests.
-#
-
-testHelp()
-{
+testHelp() {
   _testHelp '-h'
   flags_getoptIsEnh || return
   _testHelp '--help'
 }
 
-_testHelp()
-{
+_testHelp() {
   flag=$1
 
-  # test default help output
+  # Test default help output.
   th_clearReturn
   (
-    FLAGS ${flag} >"${stdoutF}" 2>"${stderrF}"
+    FLAGS "${flag}" >"${stdoutF}" 2>"${stderrF}"
     echo $? >"${returnF}"
   )
   assertTrue \
@@ -35,27 +45,27 @@ _testHelp()
   assertTrue \
       'short request for help should have produced some help output.' \
       ${grepped}
-  [ ${grepped} -ne ${FLAGS_TRUE} ] && th_showOutput
+  [ ${grepped} -ne "${FLAGS_TRUE}" ] && th_showOutput
 
-  # test proper output when FLAGS_HELP set
+  # Test proper output when FLAGS_HELP set.
   (
     FLAGS_HELP='this is a test'
-    FLAGS ${flag} >"${stdoutF}" 2>"${stderrF}"
+    FLAGS "${flag}" >"${stdoutF}" 2>"${stderrF}"
   )
   grep 'this is a test' "${stderrF}" >/dev/null
   grepped=$?
   assertTrue 'setting FLAGS_HELP did not produce expected result' ${grepped}
-  [ ${grepped} -ne ${FLAGS_TRUE} ] && th_showOutput
+  [ ${grepped} -ne "${FLAGS_TRUE}" ] && th_showOutput
 
   # test that "'" chars work in help string
   (
     DEFINE_boolean b false "help string containing a ' char" b
-    FLAGS ${flag} >"${stdoutF}" 2>"${stderrF}"
+    FLAGS "${flag}" >"${stdoutF}" 2>"${stderrF}"
   )
   grep "help string containing a ' char" "${stderrF}" >/dev/null
   grepped=$?
   assertTrue "help strings containing apostrophes don't work" ${grepped}
-  [ ${grepped} -ne ${FLAGS_TRUE} ] && th_showOutput
+  [ ${grepped} -ne "${FLAGS_TRUE}" ] && th_showOutput
 }
 
 mock_flags_columns()
@@ -63,8 +73,7 @@ mock_flags_columns()
   echo 80
 }
 
-testStandardHelpOutput()
-{
+testStandardHelpOutput() {
   flags_getoptIsStd || startSkipping
 
   DEFINE_boolean test_bool false 'test boolean' b
@@ -103,8 +112,7 @@ EOF
   th_showOutput ${r3turn} "${stdoutF}" "${stderrF}"
 }
 
-testEnhancedHelpOutput()
-{
+testEnhancedHelpOutput() {
   flags_getoptIsEnh || startSkipping
 
   DEFINE_boolean test_bool false 'test boolean' b
@@ -131,6 +139,7 @@ flags:
 EOF
   (
     _flags_columns() { mock_flags_columns; }
+    # shellcheck disable=SC2034
     FLAGS_HELP=${help};
     FLAGS -h >"${stdoutF}" 2>"${stderrF}"
   )
@@ -143,8 +152,7 @@ EOF
   th_showOutput ${differed} "${stdoutF}" "${stderrF}"
 }
 
-testNoHelp()
-{
+testNoHelp() {
   flags_getoptIsEnh || startSkipping
 
   ( FLAGS --nohelp >"${stdoutF}" 2>"${stderrF}" )
@@ -160,15 +168,11 @@ testLoggingLevel() {
   assertTrue "Unexpected default logging level = ${got}, want ${want}" "[ ${got} -eq ${want} ]"
 
   # Override the logging level, and check again.
-  flags_setLoggingLevel ${FLAGS_LEVEL_FATAL}
-  flags_setLoggingLevel ${FLAGS_LEVEL_INFO}
+  flags_setLoggingLevel "${FLAGS_LEVEL_FATAL}"
+  flags_setLoggingLevel "${FLAGS_LEVEL_INFO}"
   got=$(flags_loggingLevel) want=${FLAGS_LEVEL_INFO}
   assertTrue "Unexpected configured logging level = ${got}, want ${want}" "[ ${got} -eq ${want} ]"
 }
-
-#------------------------------------------------------------------------------
-# Suite functions.
-#
 
 oneTimeSetUp() {
   th_oneTimeSetUp
@@ -185,5 +189,6 @@ setUp() {
 }
 
 # Load and run shUnit2.
+# shellcheck disable=SC2034
 [ -n "${ZSH_VERSION:-}" ] && SHUNIT_PARENT=$0
-. ${TH_SHUNIT}
+. "${TH_SHUNIT}"
